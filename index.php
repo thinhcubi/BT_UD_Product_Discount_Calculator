2fd94af71cfb69cac6ce5ea7665c48c7f26b6d34
thinhcubi <thinhcubi27@gmail.com>
2021-05-07 13:46:06 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form method="post" action="display_discount.php">
+    <input type="text" name="ProductDescription" placeholder="Mo ta san pham"/>
+    <input type="text" name="ListPrice" placeholder="Gia niem yet cua san phan"/>
+    <input type="text" name="DiscountPercent" placeholder="Ty le chiet khau"/>
+    <input type="submit" name="submit" value="Calculate Discount"/>
+</form>
+
+</body>
+</html>
